149b78b7904ee06df0f39b831cb673236246a2a7
Ronald Yiadom <68385468+mryiadom@users.noreply.github.com>
2026-09-02 01:54:40 +0100

    initiate

---
+++ week01/practice.py
@@ -0,0 +1,14 @@
+trades = [
+    {"ticker": "AAPL", "price": 230.02, "volume": 2500, "side": "SELL"},
+    {"ticker": "MSFT", "price": 430.23, "volume": 2500, "side": "BUY"},
+    {"ticker": "JPM",  "price": 218.81, "volume": 1500, "side": "SELL"},
+    {"ticker": "XOM",  "price": 113.61, "volume": 1000, "side": "SELL"},
+    {"ticker": "JNJ",  "price": 158.15, "volume": 2500, "side": "BUY"},
+    {"ticker": "AAPL", "price": 219.86, "volume": 7500, "side": "BUY"},
+    {"ticker": "MSFT", "price": 452.56, "volume": 1500, "side": "SELL"},
+    {"ticker": "JPM",  "price": 226.68, "volume": 1000, "side": "BUY"},
+    {"ticker": "XOM",  "price": 115.70, "volume": 1000, "side": "SELL"},
+    {"ticker": "JNJ",  "price": 166.07, "volume": 750,  "side": "SELL"},
+]
+
+print(trades)
